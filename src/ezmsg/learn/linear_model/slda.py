f1feb5769d09e72195c9e2d5e1f0202847097e5c
Chadwick Boulay <chadwick.boulay@gmail.com>
2025-03-10 18:23:08 -0400

    Refactor slda using BaseStatefulTransformer

--- src/ezmsg/learn/linear_model/slda.py
+++ src/ezmsg/learn/linear_model/slda.py
@@ -3,8 +3,7 @@ import typing
 import ezmsg.core as ez
 import numpy as np
 import numpy.typing as npt
-import scipy.io as sio
-from ezmsg.util.generator import consumer
+from ezmsg.sigproc.base import BaseStatefulTransformer, processor_state, BaseTransformerUnit
 from ezmsg.util.messages.axisarray import AxisArray, slice_along_axis
 from ezmsg.util.messages.util import replace
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
@@ -12,118 +11,103 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 from ..util import ClassifierMessage
 
 
-@consumer
-def slda_decoder(
-    settings_path: str, axis: str = "time"
-) -> typing.Generator[ClassifierMessage, AxisArray, None]:
-    msg_out = ClassifierMessage(data=np.array([]), dims=[""])  # Placeholder
-    channels: typing.Union[slice, npt.NDArray] = np.s_[:]
-    b_from_mat = settings_path[-4:] == ".mat"
-
-    if b_from_mat:
-        # Expects a very specific format from a specific project. Not for general use.
-        matlab_sLDA = sio.loadmat(settings_path, squeeze_me=True)
-        weights = matlab_sLDA["weights"][1, 1:]
-        # mean = matlab_sLDA['mXtrain']
-        # std = matlab_sLDA['sXtrain']
-        # lags = matlab_sLDA['lags'] + 1
-        channels = matlab_sLDA["channels"] - 4
-        channels -= channels[0]  # Offsets are wrong somehow.
-        lda = LDA(solver="lsqr", shrinkage="auto")
-        lda.classes_ = np.asarray([0, 1])
-        lda.coef_ = np.expand_dims(weights, axis=0)
-        lda.intercept_ = matlab_sLDA["weights"][1, 0]
-    else:
-        import pickle
-
-        with open(settings_path, "rb") as f:
-            lda = pickle.load(f)
-
-    out_template: typing.Optional[ClassifierMessage] = None
-    samp_ax_idx: int = 0
-    ch_ax_idx: int = -1
-
-    while True:
-        axis_arr_in = yield msg_out
-
-        if out_template is None:
-            # Create template ClassifierMessage using first msg and lda.classes_
-            out_labels = lda.classes_.tolist()
-            zero_shape = (0, len(out_labels))
-            out_template = ClassifierMessage(
-                data=np.zeros(zero_shape, dtype=axis_arr_in.data.dtype),
-                dims=[axis, "classes"],
-                axes={
-                    axis: axis_arr_in.axes[axis],
-                    "classes": AxisArray.CoordinateAxis(
-                        data=out_labels, dims=["classes"]
-                    ),
-                },
-                labels=out_labels,
-                key=axis_arr_in.key,
-            )
-            ch_ax_idx = axis_arr_in.dims.index("ch")
-            samp_ax_idx = axis_arr_in.dims.index(axis)
-
-        X = slice_along_axis(axis_arr_in.data, channels, ch_ax_idx)
-        X = np.moveaxis(X, samp_ax_idx, 0)
-        if X.shape[0]:
-            if b_from_mat:
-                pred_probas = []
-                for samp in X:
-                    tmp = samp.flatten(order="F") * 1e-6
-                    tmp = np.expand_dims(tmp, axis=0)
-                    # tmp = (tmp - mean) / std
-                    probas = lda.predict_proba(tmp)
-                    pred_probas.append(probas)
-                pred_probas = np.concatenate(pred_probas, axis=0)
-            else:
-                # This creates a copy.
-                X = X.reshape(X.shape[0], -1)
-                pred_probas = lda.predict_proba(X)
-
-            update_ax = out_template.axes[axis]
-            update_ax.offset = axis_arr_in.axes[axis].offset
-            msg_out = replace(
-                out_template,
-                data=pred_probas,
-                axes={
-                    **out_template.axes,
-                    # `replace` will copy the minimal set of fields
-                    axis: replace(update_ax, offset=update_ax.offset),
-                },
-            )
-        else:
-            msg_out = out_template
-
-
 class SLDASettings(ez.Settings):
     settings_path: str
     axis: str = "time"
 
 
-class SLDAState(ez.State):
-    gen: typing.Generator[ClassifierMessage, AxisArray, None]
+@processor_state
+class SLDAState:
+    lda: LDA
+    out_template: typing.Optional[ClassifierMessage] = None
 
 
-class SLDA(ez.Unit):
-    SETTINGS = SLDASettings
-    STATE = SLDAState
+class SLDATransformer(BaseStatefulTransformer[SLDASettings, AxisArray, ClassifierMessage, SLDAState]):
 
-    INPUT_SIGNAL = ez.InputStream(AxisArray)
-    OUTPUT = ez.OutputStream(ClassifierMessage)
+    def _reset_state(self, message: AxisArray) -> None:
+        if self.settings.settings_path[-4:] == ".mat":
+            # Expects a very specific format from a specific project. Not for general use.
+            import scipy.io as sio
+            matlab_sLDA = sio.loadmat(self.settings.settings_path, squeeze_me=True)
+            temp_weights = matlab_sLDA["weights"][1, 1:]
+            temp_intercept = matlab_sLDA["weights"][1, 0]
 
-    def construct_generator(self):
-        self.STATE.gen = slda_decoder(
-            settings_path=self.SETTINGS.settings_path, axis=self.SETTINGS.axis
+            # Create weights and use zeros for channels we do not keep.
+            channels = matlab_sLDA["channels"] - 4
+            channels -= channels[0]  # Offsets are wrong somehow.
+            n_channels = message.data.shape[message.dims.index("ch")]
+            valid_indices = [ch for ch in channels if ch < n_channels]
+            full_weights = np.zeros(n_channels)
+            full_weights[valid_indices] = temp_weights[:len(valid_indices)]
+
+            lda = LDA(solver="lsqr", shrinkage="auto")
+            lda.classes_ = np.asarray([0, 1])
+            lda.coef_ = np.expand_dims(full_weights, axis=0)
+            lda.intercept_ = temp_intercept  # TODO: Is this supposed to be per-channel? Why the [1, 0]?
+            self.state.lda = lda
+            # mean = matlab_sLDA['mXtrain']
+            # std = matlab_sLDA['sXtrain']
+            # lags = matlab_sLDA['lags'] + 1
+        else:
+            import pickle
+            with open(self.settings.settings_path, "rb") as f:
+                self.state.lda = pickle.load(f)
+
+        # Create template ClassifierMessage using lda.classes_
+        out_labels = self.state.lda.classes_.tolist()
+        zero_shape = (0, len(out_labels))
+        self.state.out_template = ClassifierMessage(
+            data=np.zeros(zero_shape, dtype=message.data.dtype),
+            dims=[self.settings.axis, "classes"],
+            axes={
+                self.settings.axis: message.axes[self.settings.axis],
+                "classes": AxisArray.CoordinateAxis(
+                    data=np.array(out_labels), dims=["classes"]
+                ),
+            },
+            labels=out_labels,
+            key=message.key,
         )
 
-    def initialize(self):
-        self.construct_generator()
+    def _process(self, message: AxisArray) -> ClassifierMessage:
+        samp_ax_idx = message.dims.index(self.settings.axis)
+        X = np.moveaxis(message.data, samp_ax_idx, 0)
 
-    @ez.subscriber(INPUT_SIGNAL)
-    @ez.publisher(OUTPUT)
-    async def on_message(self, message: AxisArray):
-        out_msg = self.STATE.gen.send(message)
-        if out_msg.data.size:
-            yield self.OUTPUT, out_msg
+        if X.shape[0]:
+            if isinstance(self.settings.settings_path, str) and self.settings.settings_path[-4:] == ".mat":
+                # Assumes F-contiguous weights
+                pred_probas = []
+                for samp in X:
+                    tmp = samp.flatten(order="F") * 1e-6
+                    tmp = np.expand_dims(tmp, axis=0)
+                    probas = self.state.lda.predict_proba(tmp)
+                    pred_probas.append(probas)
+                pred_probas = np.concatenate(pred_probas, axis=0)
+            else:
+                # This creates a copy.
+                X = X.reshape(X.shape[0], -1)
+                pred_probas = self.state.lda.predict_proba(X)
+
+            update_ax = self.state.out_template.axes[self.settings.axis]
+            update_ax.offset = message.axes[self.settings.axis].offset
+
+            return replace(
+                self.state.out_template,
+                data=pred_probas,
+                axes={
+                    **self.state.out_template.axes,
+                    # `replace` will copy the minimal set of fields
+                    self.settings.axis: replace(update_ax, offset=update_ax.offset),
+                },
+            )
+        else:
+            return self.state.out_template
+
+
+class SLDA(BaseTransformerUnit[
+    SLDASettings,
+    AxisArray,
+    ClassifierMessage,
+    SLDATransformer
+]):
+    SETTINGS = SLDASettings
